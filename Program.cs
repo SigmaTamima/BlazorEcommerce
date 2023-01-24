569b797ba765458076485e59e404c9813d529a6d
Tamima Sigma <79128216+SigmaTamima@users.noreply.github.com>
2023-01-24 15:08:07 +0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,37 +1,84 @@
 global using ECommerce.Shared;
-global using System.Net.Http.Json;
-global using ECommerce.Client.Services.ProductService;
-global using ECommerce.Client.Services.CartService;
-global using ECommerce.Client.Services.CategoryService;
-global using ECommerce.Client.Services.AuthService;
-global using ECommerce.Client.Services.OrderService;
-global using Microsoft.AspNetCore.Components.Authorization;
-global using ECommerce.Client.Services.AddressService;
-global using ECommerce.Client.Services.ProductTypeService;
-using ECommerce.Client;
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+global using Microsoft.EntityFrameworkCore;
+global using ECommerce.Server.Data;
+global using ECommerce.Server.Services.ProductService;
+global using ECommerce.Server.Services.CategoryService;
+global using ECommerce.Server.Services.CartService;
+global using ECommerce.Server.Services.AuthService;
+global using ECommerce.Server.Services.OrderService;
+global using ECommerce.Server.Services.PaymentService;
+global using ECommerce.Server.Services.AddressService;
+global using ECommerce.Server.Services.IProductTypeService;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using BlazorEcommerce.Server.Services.ProductTypeService;
 
-using Blazored.LocalStorage;
+var builder = WebApplication.CreateBuilder(args);
 
-
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddBlazoredLocalStorage();
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<IProductService, ProductService>();
+// Add services to the container.
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IProductService , ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
 builder.Services.AddScoped<ICartService, CartService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IOrderService, OrderService>();
-builder.Services.AddScoped<IAddressService, AddressService>();
+builder.Services.AddScoped<IPaymentService, PaymentService>();
 builder.Services.AddScoped<IProductTypeService, ProductTypeService>();
-builder.Services.AddOptions();
-builder.Services.AddAuthorizationCore();
-builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+builder.Services.AddScoped<IAddressService, AddressService>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey =
+                new SymmetricSecurityKey(System.Text.Encoding.UTF8
+                .GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)),
+            ValidateIssuer = false,
+            ValidateAudience = false
+        };
+    });
 
 
+builder.Services.AddHttpContextAccessor();
 
-await builder.Build().RunAsync();
+var app = builder.Build();
+app.UseSwaggerUI();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseSwagger();
+app.UseHttpsRedirection();
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
